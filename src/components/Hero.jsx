3727da73a7d3b90631b151519d3c628404135d83
Michelle1404 <michelle.wollowski1404@gmail.com>
2023-03-11 21:28:17 +0100

    logo geht

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,14 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
+import LogoCanvas from "./canvas/logo";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className={`relative w-full h-screen`}>
       <div
         className={`absolute inset-0 max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-center gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
+        <div className='flex flex-col justify-center items-center mt-4'>
           <div className='w-5 h-5 rounded-full bg-[#39cccc]' />
           <div className='w-1 sm:h-80 h-40 blue-gradient' />
         </div>
@@ -16,13 +17,13 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white `}>
             Hi, Ich bin <span className='text-[#ff4057]'>Michelle</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} mt- text-white-100`}>
             Junior Web Developer <br className='sm:block hidden' />
             MERN-Stack
           </p>
         </div>
       </div>
-
+        <LogoCanvas/>
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[30px] h-[50px] rounded-3xl border-4 border-[#39cccc] flex justify-center items-start p-2'>
---
+++ src/components/canvas/logo.jsx
@@ -0,0 +1,57 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const LogoGltf = () => {
+  const logo = useGLTF("./logo/scene.gltf");
+
+    return (
+    <>
+    <primitive object={logo.scene} scale={0.15} position-y={-0.9} rotation-y={0} />
+    <spotLight
+        position={[-20, 50, -10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+    </>
+  );
+};
+
+const LogoCanvas = () => {
+    
+  return (
+    <Canvas
+      shadows
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 30,
+        near: 0.1,
+        far: 200,
+        position: [0.3, 5, 6],
+        
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+              <OrbitControls
+                  autoRotate
+                  autoRotateSpeed={-0.6}
+                  enableZoom={false}
+                  maxPolarAngle={Math.PI / 2}
+                  minPolarAngle={Math.PI / 2}
+
+        />
+        <LogoGltf />
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default LogoCanvas;
